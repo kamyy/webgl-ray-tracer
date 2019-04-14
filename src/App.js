6dd6de1ed4886b0610365b7a1a011e96fd1cc780
Kam Y Yip <kam.yin.yip@gmail.com>
2019-04-13 18:17:31 -0700

    To get started with node.js app use create-react-app and borrow some initial code from my other repo https://github.com/kamyy/d3d-to-webgl.

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export let GL = null;
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.TXYZ_SCALAR = 0.01;
+        this.RXYZ_SCALAR = 0.25;
+        this.lButtonDown = false;
+        this.rButtonDown = false;
+        this.lx = 0;
+        this.ly = 0;
+
+        this.onMouseUp = this.onMouseUp.bind(this);
+        this.onMouseMove = this.onMouseMove.bind(this);
+        this.onMouseDown = this.onMouseDown.bind(this);
+
+        this.rootNode = null;
+    }
+
+    render() {
+        return <div className="App">
+            <canvas id='Canvas' width='1280' height='720'>Please use a browser that supports WebGL 2</canvas> 
+            <hr/>
+            <p className='ProjectInfo'>MIT License</p>
+            <p className='ProjectInfo'><a href='https://github.com/kamyy/webgl-ray-tracer'>Project @ GitHub</a></p>
+            <p className='ProjectInfo'>Copyright &copy; 2019 <a href='mailto:kam.yin.yip@gmail.com'>Kam Y Yip</a></p>
+        </div>
+    }
+
+    componentDidMount() {
+        this.canvas = document.getElementById('Canvas');
+        GL = this.canvas.getContext("webgl2");
+
+        if (GL) {
+            this.canvas.oncontextmenu = event => event.preventDefault(); // disable right click context menu
+            this.canvas.onmousedown = this.onMouseDown;
+            window.onmousemove = this.onMouseMove;
+            window.onmouseup = this.onMouseUp;
+
+            GL.clearColor(0.392156899, 0.584313750, 0.929411829, 1.0); // cornflower blue
+            GL.clearDepth(1.0); // depth buffer clear value
+        }
+    }
+
+    onMouseUp(event) {
+        switch (event.button) {
+        case 0: this.lButtonDown = false; break;
+        case 2: this.rButtonDown = false; break;
+        default: break;
+        }
+    }
+
+    onMouseDown(event) {
+        const rect = this.canvas.getBoundingClientRect();
+        const x = event.clientX;
+        const y = event.clientY;
+
+        if (x > rect.left && x < rect.right && y > rect.top && y < rect.bottom) {
+            switch (event.button) {
+            case 0: this.lButtonDown = true; break;
+            case 2: this.rButtonDown = true; break;
+            default: break;
+            }
+            this.lx = x; 
+            this.ly = y;
+        } 
+    }
+
+    onMouseMove(event) {
+        if (this.lButtonDown || this.rButtonDown) {
+        }
+    }
+
+
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+);
+
+if (module.hot) {
+    module.hot.accept();
+}
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
