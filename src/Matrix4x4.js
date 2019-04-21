719f365560fb9fafe0a3a65cf40e9f1907cbb647
Kam Y Yip <kam.yin.yip@gmail.com>
2019-04-21 03:38:10 -0700

    Indicate array is private.

--- src/Matrix4x4.js
+++ src/Matrix4x4.js
@@ -20,15 +20,15 @@ export const _43 = 14;
 export const _44 = 15;
 
 export default class Matrix4x4 {
-    m: Array<number>;
+    _m: Array<number>;
 
     constructor(elements: Matrix4x4 | Array<number> | void) {
         if (!elements) {
-            this.m = Array.of(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
+            this._m = Array.of(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
         } else if (elements instanceof Matrix4x4) {
-            this.m = Array.from(elements.m);
+            this._m = Array.from(elements._m);
         } else if (Array.isArray(elements) && elements.length === 16) {
-            this.m = Array.from(elements);
+            this._m = Array.from(elements);
         } else {
             throw new Error('Cannot construct Matrix4x4!');
         }
@@ -73,66 +73,66 @@ export default class Matrix4x4 {
 
     mul(rhs: Matrix4x4): Matrix4x4 {
         return new Matrix4x4([
-            this.m[_11] * rhs.m[_11] + this.m[_12] * rhs.m[_21] + this.m[_13] * rhs.m[_31] + this.m[_14] * rhs.m[_41],
-            this.m[_11] * rhs.m[_12] + this.m[_12] * rhs.m[_22] + this.m[_13] * rhs.m[_32] + this.m[_14] * rhs.m[_42],
-            this.m[_11] * rhs.m[_13] + this.m[_12] * rhs.m[_23] + this.m[_13] * rhs.m[_33] + this.m[_14] * rhs.m[_43],
-            this.m[_11] * rhs.m[_14] + this.m[_12] * rhs.m[_24] + this.m[_13] * rhs.m[_34] + this.m[_14] * rhs.m[_44],
+            this._m[_11] * rhs._m[_11] + this._m[_12] * rhs._m[_21] + this._m[_13] * rhs._m[_31] + this._m[_14] * rhs._m[_41],
+            this._m[_11] * rhs._m[_12] + this._m[_12] * rhs._m[_22] + this._m[_13] * rhs._m[_32] + this._m[_14] * rhs._m[_42],
+            this._m[_11] * rhs._m[_13] + this._m[_12] * rhs._m[_23] + this._m[_13] * rhs._m[_33] + this._m[_14] * rhs._m[_43],
+            this._m[_11] * rhs._m[_14] + this._m[_12] * rhs._m[_24] + this._m[_13] * rhs._m[_34] + this._m[_14] * rhs._m[_44],
 
-            this.m[_21] * rhs.m[_11] + this.m[_22] * rhs.m[_21] + this.m[_23] * rhs.m[_31] + this.m[_24] * rhs.m[_41],
-            this.m[_21] * rhs.m[_12] + this.m[_22] * rhs.m[_22] + this.m[_23] * rhs.m[_32] + this.m[_24] * rhs.m[_42],
-            this.m[_21] * rhs.m[_13] + this.m[_22] * rhs.m[_23] + this.m[_23] * rhs.m[_33] + this.m[_24] * rhs.m[_43],
-            this.m[_21] * rhs.m[_14] + this.m[_22] * rhs.m[_24] + this.m[_23] * rhs.m[_34] + this.m[_24] * rhs.m[_44],
+            this._m[_21] * rhs._m[_11] + this._m[_22] * rhs._m[_21] + this._m[_23] * rhs._m[_31] + this._m[_24] * rhs._m[_41],
+            this._m[_21] * rhs._m[_12] + this._m[_22] * rhs._m[_22] + this._m[_23] * rhs._m[_32] + this._m[_24] * rhs._m[_42],
+            this._m[_21] * rhs._m[_13] + this._m[_22] * rhs._m[_23] + this._m[_23] * rhs._m[_33] + this._m[_24] * rhs._m[_43],
+            this._m[_21] * rhs._m[_14] + this._m[_22] * rhs._m[_24] + this._m[_23] * rhs._m[_34] + this._m[_24] * rhs._m[_44],
 
-            this.m[_31] * rhs.m[_11] + this.m[_32] * rhs.m[_21] + this.m[_33] * rhs.m[_31] + this.m[_34] * rhs.m[_41],
-            this.m[_31] * rhs.m[_12] + this.m[_32] * rhs.m[_22] + this.m[_33] * rhs.m[_32] + this.m[_34] * rhs.m[_42],
-            this.m[_31] * rhs.m[_13] + this.m[_32] * rhs.m[_23] + this.m[_33] * rhs.m[_33] + this.m[_34] * rhs.m[_43],
-            this.m[_31] * rhs.m[_14] + this.m[_32] * rhs.m[_24] + this.m[_33] * rhs.m[_34] + this.m[_34] * rhs.m[_44],
+            this._m[_31] * rhs._m[_11] + this._m[_32] * rhs._m[_21] + this._m[_33] * rhs._m[_31] + this._m[_34] * rhs._m[_41],
+            this._m[_31] * rhs._m[_12] + this._m[_32] * rhs._m[_22] + this._m[_33] * rhs._m[_32] + this._m[_34] * rhs._m[_42],
+            this._m[_31] * rhs._m[_13] + this._m[_32] * rhs._m[_23] + this._m[_33] * rhs._m[_33] + this._m[_34] * rhs._m[_43],
+            this._m[_31] * rhs._m[_14] + this._m[_32] * rhs._m[_24] + this._m[_33] * rhs._m[_34] + this._m[_34] * rhs._m[_44],
 
-            this.m[_41] * rhs.m[_11] + this.m[_42] * rhs.m[_21] + this.m[_43] * rhs.m[_31] + this.m[_44] * rhs.m[_41],
-            this.m[_41] * rhs.m[_12] + this.m[_42] * rhs.m[_22] + this.m[_43] * rhs.m[_32] + this.m[_44] * rhs.m[_42],
-            this.m[_41] * rhs.m[_13] + this.m[_42] * rhs.m[_23] + this.m[_43] * rhs.m[_33] + this.m[_44] * rhs.m[_43],
-            this.m[_41] * rhs.m[_14] + this.m[_42] * rhs.m[_24] + this.m[_43] * rhs.m[_34] + this.m[_44] * rhs.m[_44]
+            this._m[_41] * rhs._m[_11] + this._m[_42] * rhs._m[_21] + this._m[_43] * rhs._m[_31] + this._m[_44] * rhs._m[_41],
+            this._m[_41] * rhs._m[_12] + this._m[_42] * rhs._m[_22] + this._m[_43] * rhs._m[_32] + this._m[_44] * rhs._m[_42],
+            this._m[_41] * rhs._m[_13] + this._m[_42] * rhs._m[_23] + this._m[_43] * rhs._m[_33] + this._m[_44] * rhs._m[_43],
+            this._m[_41] * rhs._m[_14] + this._m[_42] * rhs._m[_24] + this._m[_43] * rhs._m[_34] + this._m[_44] * rhs._m[_44]
         ]);
     }
 
     inverse(): Matrix4x4 {
         return new Matrix4x4([
-            this.m[_11], this.m[_21], this.m[_31], 0,
-            this.m[_12], this.m[_22], this.m[_32], 0,
-            this.m[_13], this.m[_23], this.m[_33], 0,
-            -(this.m[_41] * this.m[_11] + this.m[_42] * this.m[_12] + this.m[_43] * this.m[_13]),
-            -(this.m[_41] * this.m[_21] + this.m[_42] * this.m[_22] + this.m[_43] * this.m[_23]),
-            -(this.m[_41] * this.m[_31] + this.m[_42] * this.m[_32] + this.m[_43] * this.m[_33]), 1
+            this._m[_11], this._m[_21], this._m[_31], 0,
+            this._m[_12], this._m[_22], this._m[_32], 0,
+            this._m[_13], this._m[_23], this._m[_33], 0,
+            -(this._m[_41] * this._m[_11] + this._m[_42] * this._m[_12] + this._m[_43] * this._m[_13]),
+            -(this._m[_41] * this._m[_21] + this._m[_42] * this._m[_22] + this._m[_43] * this._m[_23]),
+            -(this._m[_41] * this._m[_31] + this._m[_42] * this._m[_32] + this._m[_43] * this._m[_33]), 1
         ]);
     }
 
     postCatTxyz(tx: number, ty: number, tz: number): Matrix4x4 {
         return new Matrix4x4([
-            this.m[_11], this.m[_12], this.m[_13], this.m[_14],
-            this.m[_21], this.m[_22], this.m[_23], this.m[_24],
-            this.m[_31], this.m[_32], this.m[_33], this.m[_34],
-            this.m[_41] + tx, this.m[_42] + ty, this.m[_43] + tz, this.m[_44]
+            this._m[_11], this._m[_12], this._m[_13], this._m[_14],
+            this._m[_21], this._m[_22], this._m[_23], this._m[_24],
+            this._m[_31], this._m[_32], this._m[_33], this._m[_34],
+            this._m[_41] + tx, this._m[_42] + ty, this._m[_43] + tz, this._m[_44]
         ]);
     }
 
     postCatSxyz(sx: number, sy: number, sz: number): Matrix4x4 {
         return new Matrix4x4([
-            this.m[_11] * sx, this.m[_12] * sy, this.m[_13] * sz, this.m[_14],
-            this.m[_21] * sx, this.m[_22] * sy, this.m[_23] * sz, this.m[_24],
-            this.m[_31] * sx, this.m[_32] * sy, this.m[_33] * sz, this.m[_34],
-            this.m[_41] * sx, this.m[_42] * sy, this.m[_43] * sz, this.m[_44]
+            this._m[_11] * sx, this._m[_12] * sy, this._m[_13] * sz, this._m[_14],
+            this._m[_21] * sx, this._m[_22] * sy, this._m[_23] * sz, this._m[_24],
+            this._m[_31] * sx, this._m[_32] * sy, this._m[_33] * sz, this._m[_34],
+            this._m[_41] * sx, this._m[_42] * sy, this._m[_43] * sz, this._m[_44]
         ]);
     }
 
     toFloat32Array(): Float32Array {
-        return new Float32Array(this.m);
+        return new Float32Array(this._m);
     }
 
     toString(): string {
         return "[Matrix4x4 " + 
-                this.m[_11] + ", " + this.m[_12] + ", " + this.m[_13] + ", " + this.m[_14] + ", " + 
-                this.m[_21] + ", " + this.m[_22] + ", " + this.m[_23] + ", " + this.m[_24] + ", " + 
-                this.m[_31] + ", " + this.m[_32] + ", " + this.m[_33] + ", " + this.m[_34] + ", " +
-                this.m[_41] + ", " + this.m[_42] + ", " + this.m[_43] + ", " + this.m[_44] + "]";
+                this._m[_11] + ", " + this._m[_12] + ", " + this._m[_13] + ", " + this._m[_14] + ", " + 
+                this._m[_21] + ", " + this._m[_22] + ", " + this._m[_23] + ", " + this._m[_24] + ", " + 
+                this._m[_31] + ", " + this._m[_32] + ", " + this._m[_33] + ", " + this._m[_34] + ", " +
+                this._m[_41] + ", " + this._m[_42] + ", " + this._m[_43] + ", " + this._m[_44] + "]";
     }
 }
