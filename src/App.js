a1e77b7375668d405784aad873de8d5c992e42b6
Kam Y Yip <kam.yin.yip@gmail.com>
2019-04-21 13:04:25 -0700

    Move ray intersect sphere code to Intersect.js. Render a red test sphere on 2D canvas using rays.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React, { Component } from 'react';
+import * as React from 'react';
+
+import Vector1x4 from './Vector1x4';
+import Sphere from './Sphere';
+import Ray from './Ray';
+
+import {
+    RayIntersectSphereResult,
+    rayIntersectSphere
+} 
+from './Intersect';
 
 export let x2d = null;
 
-class App extends Component {
+class App extends React.Component {
     constructor(props) {
         super(props);
 
@@ -46,26 +56,43 @@ class App extends Component {
         </div>
     }
 
-    clearImage() {
+    testSphere() {
         if (x2d) {
             const image = x2d.createImageData(this.props.wd, this.props.ht);
-            const dy = 255 / this.props.ht;
-            const dx = 255 / this.props.wd;
 
-            let i = 0;
-            let g = 255;
-            for (let y = 0; y < this.props.ht; ++y) {
+            const halfWd = this.props.wd * 0.5;
+            const halfHt = this.props.ht * 0.5;
+            const x = -halfWd;
+            const y = +halfHt / Math.tan(45 * Math.PI / 180);
+            const z = +halfHt;
 
-                let r = 0;
-                for (let x = 0; x < this.props.wd; ++x) {
-                    image.data[i++] = r;
-                    image.data[i++] = g;
-                    image.data[i++] = 0;
-                    image.data[i++] = 255;
-                    r += dx;
+            const org = new Vector1x4();
+            const dir = new Vector1x4(x, y, z);
+            const ray = new Ray(org, dir);
+
+            const sphere = new Sphere(new Vector1x4(0, 100, 0), 10);
+
+            let k = 0;
+
+            for (let i = 0; i < this.props.ht; ++i) { // top to bottom
+                for (let j = 0; j < this.props.wd; ++j) { // left to right
+                    dir.x = x + j;
+                    dir.z = z - i;
+
+                    if (rayIntersectSphere(ray, sphere)) {
+                        image.data[k++] = 128;
+                        image.data[k++] = 0;
+                        image.data[k++] = 0;
+                        image.data[k++] = 255;
+                    } else {
+                        image.data[k++] = 0;
+                        image.data[k++] = 0;
+                        image.data[k++] = 0;
+                        image.data[k++] = 255;
+                    }
                 }
-                g -= dy;
             }
+
             x2d.putImageData(image, 0, 0);
         }
     }
@@ -80,7 +107,7 @@ class App extends Component {
             window.onmousemove = this.onMouseMove;
             window.onmouseup = this.onMouseUp;
 
-            this.clearImage();
+            this.testSphere();
 
             //x2d.getSupportedExtensions().forEach(item => console.log(item));
             //GL.clearColor(0.392156899, 0.584313750, 0.929411829, 1.0); // cornflower blue
--- src/Sphere.js
+++ src/Sphere.js
@@ -1,6 +1,5 @@
 // @flow
 import Vector1x4 from './Vector1x4.js';
-import Ray from './Ray.js'
 
 export default class Sphere {
     _center:        Vector1x4;
@@ -32,15 +31,5 @@ export default class Sphere {
 
     get radiusSquared(): number {
         return this._radiusSquared;
-    }
-
-    hitTest(ray: Ray): boolean {
-        const l = ray.origin.sub(this._center);
-        const a = ray.dir.dot(ray.dir);
-        const b = ray.dir.dot(l) * 2;
-        const c = l.dot(l) - this._radiusSquared;
-        const discriminant = (b * b) - (4 * a * c);
-
-        return (discriminant > 0);
     }
 }
