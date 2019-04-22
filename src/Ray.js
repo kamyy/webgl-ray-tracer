4886d2c57a0189f1f87bb858908eeeea5120a55f
Kam Y Yip <kam.yin.yip@gmail.com>
2019-04-21 17:32:32 -0700

    Simplify Ray and Sphere classes.

--- src/Ray.js
+++ src/Ray.js
@@ -2,31 +2,15 @@
 import Vector1x4 from './Vector1x4.js';
 
 export default class Ray {
-    _origin: Vector1x4;
-    _dir:    Vector1x4;
+    origin: Vector1x4;
+    dir:    Vector1x4;
 
     constructor(origin: Vector1x4, dir: Vector1x4) {
-        this._origin = origin;
-        this._dir    = dir;
-    }
-
-    get origin(): Vector1x4 {
-        return this._origin;
-    }
-
-    set origin(v: Vector1x4) {
-        this._origin = v;
-    }
-
-    get dir(): Vector1x4 {
-        return this._dir;
-    }
-
-    set dir(v: Vector1x4) {
-        this._dir = v;
+        this.origin = origin;
+        this.dir    = dir;
     }
 
     getPos(t: number): Vector1x4 {
-        return this._origin.add(this._dir.mul(t));
+        return this.origin.add(this.dir.mul(t));
     }
 }
--- src/Sphere.js
+++ src/Sphere.js
@@ -2,34 +2,15 @@
 import Vector1x4 from './Vector1x4.js';
 
 export default class Sphere {
-    _center:        Vector1x4;
-    _radius:        number;
-    _radiusSquared: number;
+    center:        Vector1x4;
+    radius:        number;
 
     constructor(center: Vector1x4, radius: number) {
-        this._center        = center;
-        this._radius        = radius;
-        this._radiusSquared = radius * radius;
-    }
-
-    get center(): Vector1x4 {
-        return this._center;
-    }
-
-    set center(center: Vector1x4) {
-        this._center = center;
-    }
-
-    get radius(): number {
-        return this._radius;
-    }
-
-    set radius(radius: number) {
-        this._radius        = radius;
-        this._radiusSquared = radius * radius;
+        this.center = center;
+        this.radius = radius;
     }
 
     get radiusSquared(): number {
-        return this._radiusSquared;
+        return this.radius * this.radius;
     }
 }
