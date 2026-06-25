47bf6d55f6548f0f409c0ed169ed3d661f829702
Kam Y Yip <kam.yin.yip@gmail.com>
2026-06-25 13:25:32 -0700

    Simplify GA (#11)

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import Canvas from '@/components/canvas'
 import Forms from '@/components/forms'
 import { Progress } from '@/components/progress'
-import { initializeGoogleAnalytics } from '@/lib/analytics/googleAnalytics'
 import StoreProvider from '@/components/storeProvider'
 import { defaultCanvasVars } from '@/types/canvasVars'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/globals.css'
-
-initializeGoogleAnalytics()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
